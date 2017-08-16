d78913113cd7b9a2375f8e0b067848184e9842a0
Noesis Mik <sharp7c0timesfour@gmail.com>
2017-08-16 21:55:46 +0900

    인증 구현중

--- server.js
+++ server.js
@@ -35,8 +35,6 @@ function createRenderer (bundle, options) {
 
 function render (request, reply) {
 
-  console.log("render request", request.auth.credentials);
-
   const context = { url: request.params.param, session: request.auth.credentials };
 
   renderer.renderToString(context, (err, html) => {
@@ -68,59 +66,12 @@ function startServer () {
   }
 }
 
-function oAuthRequest (uri, method, options) {
-  const url          = uri;
-  const oauthOptions = {
-    oauth_consumer_key    : "bGKjn0l5Zu92zTBRruN1U8YCo",
-    oauth_nonce           : Cryptiles.randomString(9),
-    oauth_timestamp       : Math.floor(Date.now() / 1000).toString(),
-    oauth_signature_method: "HMAC-SHA1",
-    oauth_version         : "1.0",
-  };
-
-  const realOptions = Object.assign({}, oauthOptions, options);
-
-  const encodedSignature = oauthSignature.generate(method, url, realOptions, "GcdeXsQccarT66eMgVzG9pG8WUPu0XWvHLKw3icyFcd9vpL57G", null, {
-    encodeSignature: false
-  });
-  
-  let finalParam = Object.assign({}, realOptions, {
-    oauth_signature: encodedSignature
-  });
-
-  console.log("SDFSDFSDF", finalParam);
-
-  const paramString = Object.keys(finalParam).map((m) => {
-    return `${m}="${encodeURIComponent(finalParam[m])}"`;
-  }).join(",");
-
-  return {
-    "Content-Type" : "application/x-www-form-urlencoded",
-    "Authorization": `OAuth ${paramString}`,
-  };
-}
-
-function authRequest(request, reply) {
-  if (!request.auth.isAuthenticated) {
-    reply(null).code(401);
-  } else {
-
-  }
-}
-
 // register plugin
-server.register(Inert);
-
-server.register([AuthCookie, Bell], (err) => {
+server.register([AuthCookie, Bell, Inert], (err) => {
 
   // auth information setting
   server.auth.strategy("session", "cookie", {
     password  : 'a7d5c3c8-7fba-11e7-bb31-be2e44b06b34',
-    //redirectTo: '/auth/twitter',
-   // redirectTo: '/',
-    // validateFunc (...args) {
-    //   console.log("!!!!", args);
-    // },
     isSameSite: "Lax",
     isSecure  : false
   });
@@ -155,10 +106,32 @@ server.register([AuthCookie, Bell], (err) => {
     }
   });
 
+  server.route({
+    method: "GET",
+    path  : "/{param*}",
+    config: {
+      
+      auth: {
+        strategy: 'session',
+        mode    : 'optional'
+      },
+
+      handler: function (request, reply) {
+        if(isProd) {
+          render(request, reply);
+        } else {
+          readyPromise.then(() => {
+            render(request, reply);
+          })
+        }
+      }
+    },
+  });
+
   // twitter auth
   server.route({
     method: 'GET',
-    path: '/auth/twitter',
+    path  : '/auth/twitter',
     config: {
       auth   : 'twitter',
       handler: function(request, reply) {
@@ -168,18 +141,6 @@ server.register([AuthCookie, Bell], (err) => {
         }
 
         const profile = request.auth.credentials.profile;
-
-        //console.log("!!!!", request.auth.credentials);
-
-        // request.server.app.cache.set(sid, { account: account }, 0, (err) => {
-
-        //     if (err) {
-        //         reply(err);
-        //     }
-
-        //     request.cookieAuth.set({ sid: sid });
-        //     return reply.redirect('/');
-        // });
 
         request.cookieAuth.set({
           twitterId  : profile.id,
@@ -192,52 +153,21 @@ server.register([AuthCookie, Bell], (err) => {
     }
   });
 
+  // logout
   server.route({
     method : "GET",
     path   : "/logout",
     config: {
-      //auth: false,
       handler: function (request, reply) {
-        console.log("!!!!!", request.auth);
         request.cookieAuth.clear();
-        //request.auth.session.clear();
         reply.redirect('/');
       }
     }
   });
 
-  // server.route({
-  //   method: "GET",
-  //   path  : "/"
-  // });
-
-  server.route({
-    method: "GET",
-    path  : "/{param*}",
-    config: {
-      //auth   : "session",
-      auth: {
-            strategy: 'session',
-            mode: 'optional'
-        },
-      handler: function (request, reply) {
-        //console.log("!!!!", request.auth.credentials.displayName);
-        //return reply('Hello, ' + request.auth.credentials.displayName + '!');
-        if(isProd) {
-          render(request, reply);
-        } else {
-          readyPromise.then(() => {
-            render(request, reply);
-          })
-        }
-      }
-    },
-  });
-
   // start server
 
   if (isProd) {
-
     const bundle         = require('./dist/vue-ssr-server-bundle.json');
     const clientManifest = require('./dist/vue-ssr-client-manifest.json');
 
